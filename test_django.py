a441933704ace4dd960eace7e05d6ae9d1cf065e
khazidhea <azamat@galimzhanov.com>
2020-06-18 15:44:05 +0600

    Add env variable check for tests

--- test_django.py
+++ test_django.py
@@ -1,13 +1,17 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
 def test_basic(live_server):
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+    if os.environ.get('GITHUB_ACTIONS'):
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+    else:
+        driver = webdriver.Remote(command_executor='http://192.168.96.1:9515')
 
     driver.get(live_server.url)
     counter = driver.find_element_by_id('counter')
