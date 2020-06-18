76736c635fa9dafd5116fd8da491e4407f04c913
khazidhea <azamat@galimzhanov.com>
2020-06-18 15:09:47 +0600

    Use headless Chrome

--- test_django.py
+++ test_django.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def test_basic(live_server):
-    driver = webdriver.Firefox()
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
 
     driver.get(live_server.url)
     counter = driver.find_element_by_id('counter')
